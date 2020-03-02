69cb097b029f530323a22b40f2349c5c4ec87874
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-03-03 08:09:20 +0900

    うｐ

--- resources/views/entries/home.blade.php
+++ resources/views/entries/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.after-login')
 
-@section('content')
+
 <div id="home" class="big-bg">
   @component('components.header')
   @endcomponent
@@ -8,4 +8,4 @@
         <h2>あなたの好きなYoutuberの軌跡を投稿しよう</h2>
     </div><!-- /.home-content -->
   </div><!-- /#home -->
-@endsection
+
--- resources/views/layouts/after-login.blade.php
+++ resources/views/layouts/after-login.blade.php
@@ -3,14 +3,25 @@
   <head>
   <meta charset="utf-8">
   <title>WW</title>
-        <meta name="description" content="ブレンドコーヒーとヘルシーなオーガニックフードを提供するカフェ">
-        <link rel="icon" type="image/png" href="img/favicon.png">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- <title>{{ config('app.name', 'Laravel') }}</title> -->
+    <meta name="description" content="あなたの好きなYoutuberの軌跡を投稿しよう">
+    <link rel="icon" type="image/png" href="img/favicon.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSS -->
-        <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-        <link href="https://fonts.googleapis.com/css?family=Philosopher" rel="stylesheet">
-        <link href="/css/after-login.css" rel="stylesheet">
+  <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+  <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+  <!-- CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Philosopher" rel="stylesheet">
+    <link href="/css/after-login.css" rel="stylesheet">
   </head>
   <body>
     @yield('content')
