27c0141f7f885da462e67f8e5e6d3db990ca902a
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-03-15 13:49:17 +0900

    up

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/images', 'ImageController@index');
+Route::post('/images', 'ImageController@store')->name('image.store');
